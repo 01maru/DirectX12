65ba1d9c492b0b0b8caecd935a79f7db7464b8ad
YukiMaruichi <k021g1419@g.neec.ac.jp>
2023-04-12 00:43:58 +0900

    easing修正

--- Engine/math/Easing.cpp
+++ Engine/math/Easing.cpp
@@ -14,10 +14,12 @@ T Easing::EaseInOut(T startpos, T endpos, T time, int num)
 {
 	if (time < 0.5)
 	{
-		return lerp(startpos, endpos, (pow(2, num - 1) * pow(time, num)));
+		return lerp(startpos, endpos, (T)(pow(2, num - 1) * pow(time, num)));
 	}
-	return lerp(startpos, endpos, (1 - pow(-2 * time + 2, num) / 2));
+	return lerp(startpos, endpos, (T)(1 - pow(-2 * time + 2, num) / 2));
 }
+template float Easing::EaseInOut<float>(float, float, float, int);
+template double Easing::EaseInOut<double>(double, double, double, int);
 
 template<typename T>
 T Easing::EaseInBack(T startpos, T endpos, T time, int num, double p1)
@@ -25,18 +27,21 @@ T Easing::EaseInBack(T startpos, T endpos, T time, int num, double p1)
 	const double c1 = p1;
 	const double c3 = c1 + 1;
 
-	return lerp(startpos, endpos, pow((c3 * time * time * time - c1 * time * time), num));
+	return lerp(startpos, endpos, (T)pow((c3 * time * time * time - c1 * time * time), num));
 }
+template float Easing::EaseInBack<float>(float, float, float, int, double);
+template double Easing::EaseInBack<double>(double, double, double, int, double);
 
 template<typename T>
 T Easing::EaseOutBack(const T startpos, const T endpos, T time, int num, double p1)
 {
-	if (time < 0.5)
-	{
-		return lerp(startpos, endpos, (pow(2, num - 1) * pow(time, num)));
-	}
-	return lerp(startpos, endpos, (1 - pow(-2 * time + 2, num) / 2));
+	double c1 = p1;
+	double c3 = c1 + 1;
+
+	return lerp(startpos, endpos, (T)pow(1 + c3 * pow(time - 1, 3) + c1 * pow(time - 1, 2), num));
 }
+template float Easing::EaseOutBack<float>(float, float, float, int, double);
+template double Easing::EaseOutBack<double>(double, double, double, int, double);
 
 template<typename T>
 T Easing::EaseIn(T start, T end, T time, int num)
@@ -49,5 +54,7 @@ template double Easing::EaseIn<double>(double, double, double, int);
 template<typename T>
 T Easing::EaseOut(T start, T end, T time, int num)
 {
-	return lerp(start, end, 1 - pow(1 - time, num));
+	return lerp(start, end, (T)(1 - pow(1 - time, num)));
 }
+template float Easing::EaseOut<float>(float, float, float, int);
+template double Easing::EaseOut<double>(double, double, double, int);
